cf9f1974e7a9f46478c6f389f7ea8b90653b7920
akjayant <ashish.jayant94@gmail.com>
2020-10-08 07:44:02 +0530

    DQN

--- DQN/train.py
+++ DQN/train.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn.functional as F
 from collections import namedtuple
 from tqdm import tqdm
+from itertools import count
 
 class Agent():
     def __init__(self,action_space,frame_history_len,env,device,buffer_size,\
@@ -15,8 +16,8 @@ class Agent():
         self.frame_history_len = frame_history_len
         self.env = env
         self.device = device
-        self.policy_qnet = model.DQN(self.frame_history_len,84,84,6,1e-3).to(self.device)
-        self.target_qnet = model.DQN(self.frame_history_len,84,84,6,1e-3).to(self.device)
+        self.policy_qnet = model.DQN(self.frame_history_len,84,84,6,1e-4).to(self.device)
+        self.target_qnet = model.DQN(self.frame_history_len,84,84,6,1e-4).to(self.device)
         self.target_qnet.load_state_dict(self.policy_qnet.state_dict())
         self.optimizer = self.policy_qnet.optimizer
         self.epsilon_start = epsilon_start
@@ -40,6 +41,7 @@ class Agent():
             with torch.no_grad():
                 action_values = self.policy_qnet(state.to(self.device))
             #----choose best action
+            action = action_values.max(1)[1].view(1,1)
             action = np.argmax(action_values.cpu().data.numpy())
             #----We need switch it back to training mode
             self.policy_qnet.train()
@@ -52,6 +54,7 @@ class Agent():
         return state.unsqueeze(0)
 
     def update_gradients(self):
+        gamma = 0.99
         if self.buffer.__len__()<self.batch_size:
             return
         batch = self.buffer.sample(self.batch_size)
@@ -79,7 +82,7 @@ class Agent():
         action_values = self.target_qnet(next_states).detach()
         #print(action_values.max(1)[0].detach())
         max_action_values = action_values.max(1)[0].detach()
-        target = rewards + max_action_values*(1-dones)
+        target = rewards + gamma*max_action_values*(1-dones)
         current = self.policy_qnet(states).gather(1,actions)
         target = target.reshape(32,1)
         #print(target.size())
@@ -90,17 +93,18 @@ class Agent():
         self.optimizer.zero_grad()
         loss.backward()
         for param in self.policy_qnet.parameters():
-            param.grad.data.clamp_(-1, 1)
+            param.grad.data.clamp_(-1.2, 1.2)
         self.optimizer.step()
 
 
     def train(self,max_epsiodes,max_steps):
+        global steps
         eps = self.epsilon_start
         for episode in tqdm(range(max_epsiodes)):
             obs = self.env.reset()
             state = self.torchify_state_dim(obs)
             total_reward = 0
-            for t in range(max_steps):
+            for t in  count():
                 action = self.epsilon_greedy_act(state,eps)
                 next_state,reward,done,_ = self.env.step(action)
                 if done:
@@ -114,30 +118,33 @@ class Agent():
 
                 self.buffer.add(state,action,next_state,reward.to('cpu'),done_flag)
                 eps = max(eps * self.epsilon_decay, self.epsilon_min)
+                steps += 1
                 #print(self.buffer.__len__())
-                if self.buffer.__len__() > 10000:
+                if steps > 10000:
                     self.update_gradients()
-                    if t%self.update_every==0:
+                    if steps%self.update_every==0:
                         self.target_qnet.load_state_dict(self.policy_qnet.state_dict())
                 state = next_state
                 if done:
                     break
-            if episode%50 == 0:
+            if episode%10 == 0:
                 print("Episode no "+str(episode)+" reward = "+str(total_reward))
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    action_space = 6
+    action_space = 4
     frame_history_len = 4
     env = gym.make("PongNoFrameskip-v4")
     env = make_env(env)
+    steps = 0
     buffer_size = 100000
     epsilon_start = 1
-    epsilon_decay = 0.999
-    epsilon_min = 0.001
+    epsilon_decay = 0.99
+    epsilon_min = 0.01
     update_every = 1000
     batch_size = 32
     myagent = Agent(action_space,frame_history_len,env,device,buffer_size,\
     epsilon_start,epsilon_decay,epsilon_min,update_every,batch_size)
     myagent.train(500,100000)
     torch.save(myagent.policy_qnet, "saved_model")
+    env.close()
